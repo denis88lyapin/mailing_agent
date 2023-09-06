c097345071204fe8ece9de07aa56489e36896194
Denis_L <denis88lyapin@gmail.com>
2023-09-06 22:29:06 +0300

    Добавлен CRUD для Client

--- agent/urls.py
+++ agent/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from agent.apps import AgentConfig
-from agent.views import MailingListView, MailingCreateView, MailingUpdateView, MailingDetailView, MailingDeleteView
+from agent.views import (MailingListView, MailingCreateView, MailingUpdateView, MailingDetailView, MailingDeleteView,
+                         ClientListView, ClientCreateView, ClientUpdateView, ClientDetailView, ClientDeleteView)
 
 app_name = AgentConfig.name
 
@@ -10,4 +11,9 @@ urlpatterns = [
     path('mailing/<int:pk>/update/', MailingUpdateView.as_view(), name='mailing_update'),
     path('mailing/<int:pk>/detail/', MailingDetailView.as_view(), name='mailing_detail'),
     path('mailing/<int:pk>/delete/', MailingDeleteView.as_view(), name='mailing_delete'),
+    path('client/', ClientListView.as_view(), name='client_list'),
+    path('client_create/', ClientCreateView.as_view(), name='client_create'),
+    path('client/<int:pk>/update/', ClientUpdateView.as_view(), name='client_update'),
+    path('client/<int:pk>/detail/', ClientDetailView.as_view(), name='client_detail'),
+    path('client/<int:pk>/delete/', ClientDeleteView.as_view(), name='client_delete'),
 ]
