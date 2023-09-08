f79783f2b10136fd404e68681c3d560d8f73fc59
Denis_L <denis88lyapin@gmail.com>
2023-09-08 22:30:24 +0300

    Добавлен для приложения users

---
+++ users/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from users.apps import UsersConfig
+from users.views import (LoginView, LogoutView, RegisterView, ProfileView, generate_new_password,
+                         ConfirmRegistrationView, reset_password)
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('profile/genpassword/', generate_new_password, name='generate_new_password'),
+    path('confirm/<str:vrf_token>/', ConfirmRegistrationView.as_view(), name='confirm'),
+    path('reset_password', reset_password, name='reset_password'),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,115 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView as BaseLoginView
+from django.contrib.auth.views import LogoutView as BaseLogoutView
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+from django.views import View
+from django.views.generic import CreateView, UpdateView
+from config import settings
+from users.forms import UserForm, UserProfileForm
+from users.models import User
+from django.contrib import messages
+
+
+class LoginView(BaseLoginView):
+    template_name = 'users/login.html'
+
+
+class LogoutView(BaseLogoutView):
+    pass
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserForm
+    success_url = reverse_lazy('users:login')
+    template_name = 'users/register.html'
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        vrf_token = ''.join([str(random.randint(0, 9)) for _ in range(12)])
+        confirm_url = self.request.build_absolute_uri(reverse('users:confirm', args=[vrf_token]))
+        user.vrf_token = vrf_token
+        user.save()
+        to = user.email
+        subject = 'Регистрация в нашем проекте E-shop!'
+        message = f'Для регистрации перейдите по ссылке {confirm_url}!'
+        from_email = settings.EMAIL_HOST_USER
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=from_email,
+            recipient_list=[to]
+        )
+        return super().form_valid(form)
+
+
+class ConfirmRegistrationView(View):
+    def get(self, request, vrf_token):
+        try:
+            user = User.objects.get(vrf_token=vrf_token)
+            user.is_active = True
+            user.vrf_token = None
+            user.save()
+        except User.DoesNotExist:
+            messages.error(request, "Ошибка: Пользователь с указанным "
+                                    "токеном не найден. Пройдите регистрацию снова.")
+        return redirect('users:login')
+
+
+class ProfileView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+@login_required
+def generate_new_password(request):
+    new_password = ''.join([str(random.randint(0, 9)) for _ in range(12)])
+    to = request.user.email
+    subject = 'Вы сменили пароль!'
+    message = f'Ваш новый пароль: {new_password}'
+    from_email = settings.EMAIL_HOST_USER
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=from_email,
+        recipient_list=[to]
+    )
+    request.user.set_password(new_password)
+    request.user.save()
+    return redirect(reverse('catalog:home'))
+
+
+# @login_required
+def reset_password(request):
+    """Сгенерировать новый пароль для пользователя если пароль забыли"""
+    if request.method == 'POST':
+        user_email = request.POST.get('email')
+        try:
+            user = User.objects.get(email=user_email)
+            new_password = "".join([str(random.randint(0, 9)) for _ in range(12)])
+            user.set_password(new_password)
+            user.save()
+
+            subject = "Смена пароля на платформе E-shop"
+            message = f"Ваш новый пароль: {new_password}"
+            from_email = settings.EMAIL_HOST_USER
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=from_email,
+                recipient_list=[user_email]
+            )
+            return redirect(reverse("users:login"))
+        except User.DoesNotExist:
+            return render(request, 'users/change_password.html', {'error_message': 'User not found'})
+    return render(request, 'users/change_password.html')
